a63eb96cad148d767dc9b233a48f29c376ec5d10
Diego Suarez <1412612@utp.edu.pe>
2022-03-15 21:03:02 -0500

    enviando archivo macros

---
+++ Macros_Funciones.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+#define operacion(x)    (x*x + 2*x  -1)
+
+#define imprimir(exp)   if(exp > 2){\
+                            printf(#exp" = %.2f\n",exp);\
+                            printf("fin\n");} \
+                        else{\
+                            printf("fuera de rango\n");\
+                            printf("fin\n");} \   
+                        
+#define concatenar(x,y)    x##y                                                          
+
+
+int main(){
+    float x=1;
+    float y=2;
+    char a='h';
+    char b='J';
+    char valor[3];
+    printf("El valor de x ahora es: %.2f\n",operacion(x));
+    imprimir(x);
+    printf("El valor concatenado es: %.d\n",concatenar(2,3));
+    return 0;
+}
